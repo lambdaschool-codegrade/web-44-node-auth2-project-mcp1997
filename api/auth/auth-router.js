f5af912ce9000c11a383edf6159b52cbd94b2e6c
Matt Phillips <mattcphillips17@gmail.com>
2021-08-17 21:46:00 -0700

    register working

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
+
+const bcrypt = require('bcryptjs')
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+
+const User = require('../users/users-model')
 
 router.post("/register", validateRoleName, (req, res, next) => {
   /**
@@ -14,6 +18,15 @@ router.post("/register", validateRoleName, (req, res, next) => {
       "role_name": "angel"
     }
    */
+  const { username, password } = req.body
+  const { role_name } = req
+  const hash = bcrypt.hashSync(password, 8)
+  
+  User.add({ username, password: hash, role_name })
+    .then(noidea => {
+      res.status(201).json(noidea)
+    })
+    .catch(next)
 });
 
 
